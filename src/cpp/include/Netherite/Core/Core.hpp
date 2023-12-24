a85c06b9b2fab21ef11506620db040a951afbead
unit-404 <elviras9t@gmail.com>
2023-12-24 17:55:57 +0000

    under-construction

---
+++ src/cpp/include/Netherite/Core/Core.hpp
@@ -0,0 +1,174 @@
+#pragma once
+
+//
+#ifdef NTH_IMPLEMENTATION
+#undef NTH_IMPLEMENTATION
+// push headers without implementation
+#define NTH_IMPLEMENTATION
+#else
+#include <cstring>
+#include <cstdint>
+#include <unordered_map>
+#include <memory>
+// push headers for headers
+#endif
+//
+
+//
+#ifdef NTH_IMPLEMENTATION
+#include "./Handle.hpp"
+#endif
+
+//
+namespace nth {
+    //
+    class Handle;
+    class RegistryMemberBase;
+}
+
+//
+template<> struct std::hash<nth::Handle> {
+    inline std::size_t operator()(const nth::Handle& s) const noexcept;
+};
+
+//
+namespace nth {
+    #ifndef NTH_IMPLEMENTATION
+    // header
+
+    //
+    using reference_t = uintptr_t;
+
+    //
+    class Handle {
+        public: uintptr_t addressOrId = 0ull;
+        public: uint32_t typeOf = 0;
+
+        //
+        public: Handle(uintptr_t const& addressOrId, uint32_t const& typeOf) {
+            this->addressOrId = addressOrId;
+            this->typeOf = typeOf;
+        }
+        
+        //
+        public: Handle(Handle const& handle = Handle(0ull, 0u)) {
+            this->addressOrId = handle.addressOrId;
+            this->typeOf = handle.typeOf;
+        }
+
+        //
+        public: Handle& operator=(Handle const& handle) {
+            this->addressOrId = handle.addressOrId;
+            this->typeOf = handle.typeOf;
+            return *this;
+        }
+
+        //
+        public: bool operator==(Handle const& handle) const {
+            return (handle.addressOrId == this->addressOrId && handle.typeOf == this->typeOf);
+        }
+
+        //
+        public: operator size_t() {
+            return std::hash<Handle>{}(*this);
+        }
+
+        //
+        public: operator bool() const {
+            return this->addressOrId && this->typeOf;
+        }
+
+        //
+        public: template<class T> T& as() { return reinterpret_cast<T&>(this->addressOrId); }
+        public: template<class T> T const& as() const { return reinterpret_cast<T const&>(this->addressOrId); }
+    };
+
+    //
+    class BaseDataObj : public std::enable_shared_from_this<BaseDataObj> {
+        public: BaseDataObj() {};
+    };
+
+    //
+    template<class T = BaseDataObj>
+    class RegistryMember;
+
+    //
+    class RegistryMemberBase : public std::enable_shared_from_this<RegistryMemberBase> {
+        protected: 
+        std::weak_ptr<Handle> handle = {};
+        std::shared_ptr<Handle> base = {};
+        std::shared_ptr<BaseDataObj> data = {};
+
+        //
+        public: RegistryMemberBase(std::shared_ptr<Handle> base = {}, std::shared_ptr<Handle> handle = {}): base(base), handle(handle) {
+            
+        }
+
+        //
+        public: template<class T = BaseDataObj>
+        RegistryMember<T> inline as() const {
+            return std::reinterpret_pointer_cast<T>(shared_from_this());
+        }
+
+        //
+        public: template<class T = BaseDataObj>
+        inline std::shared_ptr<T> getData() const {
+            return std::reinterpret_pointer_cast<T>(this->data);
+        }
+
+        //
+        public: inline static std::shared_ptr<Handle> create() {
+            return std::make_shared<Handle>();
+        }
+
+        //
+        public: template<class H = uintptr_t>
+        inline static std::shared_ptr<H> create() {
+            return std::make_shared<H>((H)0ull);
+        }
+
+        //
+        public: inline std::shared_ptr<Handle> getHandle() const {
+            return this->handle.lock();
+        }
+
+        //
+        public: inline std::shared_ptr<Handle> getBase() const {
+            return this->base;
+        }
+    };
+
+    //
+    template<class T>
+    class RegistryMember: public RegistryMemberBase {
+        
+        //
+        public: inline std::shared_ptr<T> getData() const {
+            return std::reinterpret_pointer_cast<T>(this->data);
+        }
+
+        
+    };
+
+    // for buffer, image, etc.
+    template<class K = uintptr_t, class T = RegistryMemberBase>
+    using weak_map_t = std::unordered_map<std::weak_ptr<K>, T, std::owner_less<std::weak_ptr<K>>>;
+
+    // for device, commandbuffer, instance, queue, etc.
+    template<class T = RegistryMemberBase> 
+    using registry_t = std::unordered_map<Handle, T>;
+
+    //
+    #else
+    // implementation
+
+    #endif
+}
+
+//
+#ifndef NTH_IMPLEMENTATION
+inline std::size_t std::hash<nth::Handle>::operator()(const nth::Handle& s) const noexcept {
+    auto h1 = std::hash<uintptr_t>{}(s.addressOrId), h2 = std::hash<uintptr_t>{}(s.typeOf);
+    return h1 ^ (h2 << 1); // or use boost::hash_combine
+};
+#endif
